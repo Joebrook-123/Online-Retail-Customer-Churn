7937c74acf3128cdd801a8c57099fb11a1cb16ab
joebrook-123 <josefbrook1@gmail.com>
2026-09-04 18:08:25 +0100

    Outputted results as hyper file for Tableau

--- predict.py
+++ predict.py
@@ -1,30 +1,17 @@
 import pandas as pd
 from train import train_model
 from transform import train_test_split, transform
+import pantab
 
 df = pd.read_csv('online_retail_cleaned.csv', index_col=False)
+df['snapshot'] = pd.to_datetime(df['snapshot'])
 
 X_train, y_train, X = train_test_split(df)
 X = transform(X)    
 
 model = train_model(X_train, y_train)
-
 y_proba = model.predict_proba(X)[:, 1]
-
 df['churn_probability'] = y_proba.ravel()
-
-# Defining categories of risk based on percentiles
-df['pct_rank'] = df.groupby('snapshot')['churn_probability'].rank(method='first', pct=True)
-
-def map_risk(pct):
-    if pct >= 0.70:
-        return 'High Risk'
-    elif pct >= 0.30:
-        return 'Medium Risk'
-    else:
-        return 'Low Risk'
-
-df['risk_category'] = df['pct_rank'].apply(map_risk)
 
 # Cross joining data for visualisation in Tableau
 unique_customers = df['customer_id'].drop_duplicates()
@@ -43,9 +30,50 @@ df = pd.merge(
     on=['customer_id', 'snapshot'],
     how='left'
 )
-df = df.fillna(0) 
 
-# 3. Clean up the temporary column (optional)
+# Defining categories of risk based on percentiles
+df['pct_rank'] = df.groupby('snapshot')['churn_probability'].rank(method='first', pct=True)
+
+def map_risk(pct):
+    if pct >= 0.70:
+        return 'High Risk'
+    elif pct >= 0.30:
+        return 'Medium Risk'
+    else:
+        return 'Low Risk'
+
+df['risk_category'] = df['pct_rank'].apply(map_risk)
 df.drop(columns=['pct_rank'], inplace=True)
 
-df.to_csv('customer_churn_predictions.csv', index=False)
+numeric_col = [
+    'total_order_value',
+    'total_orders_placed',
+    'historic_avg_order_value',
+    'total_discounts',
+    'total_orders_returned',
+    'unique_products_purchased',
+    'total_discount_value',
+    'total_returned_value',
+    'snapshot_customer_count',
+    'snapshot_orders_placed',
+    'snapshot_avg_order_value',
+    'snapshot_total_discounts',
+    'snapshot_customer_orders',
+    'snapshot_unique_products',
+    'snapshot_customer_revenue',
+    'snapshot_returned_value',
+    'customer_churn_status',
+    'churn_probability'
+]
+
+for col in numeric_col:
+    if col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0)
+
+# Hyper files do not support 32bit floats
+float_cols = df.select_dtypes(include=['float32']).columns
+df[float_cols] = df[float_cols].astype('float64')
+
+# Export results to a Tableau hyper file
+pantab.frame_to_hyper(df, 'customer_churn_predictions.hyper', table='churn_data')
+print("Successfully generated Tableau Hyper extract!")
